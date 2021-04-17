acbf7ea568c5092290d3d61741c69a95a4446ce4
Ziad Bougrine <ziadbougrine@gmail.com>
2021-04-17 18:53:43 +0000

    New

--- DeliberationNewVersion/src/main/java/com/ziad/administrateur/structures/StructureController.java
+++ DeliberationNewVersion/src/main/java/com/ziad/administrateur/structures/StructureController.java
@@ -1,55 +1,35 @@
 package com.ziad.administrateur.structures;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.EntityNotFoundException;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.ziad.models.Etape;
-import com.ziad.models.Historique;
-import com.ziad.models.Semestre;
-import com.ziad.repositories.EtapeRepository;
-import com.ziad.repositories.EtudiantRepository;
-import com.ziad.repositories.FiliereRepository;
-import com.ziad.repositories.HistoriqueRepository;
-import com.ziad.repositories.InscriptionAdministrativeRepository;
-import com.ziad.repositories.InscriptionEnLigneRepository;
-import com.ziad.repositories.InscriptionPedagogiqueModuleRepository;
-import com.ziad.repositories.InscriptionPedagogiqueRepository;
-import com.ziad.repositories.ModuleRepository;
-import com.ziad.repositories.SemestreRepository;
+import com.ziad.administrateur.etablissement.DataNotFoundExceptions;
 
 @Controller
+@RequestMapping("/admin")
 public class StructureController {
 	
+	private final static String ATTRIBUT_LIST_ETAPES = "etapes";
+	private final static String ATTRIBUT_LIST_SEMESTRES = "semestres";
+	private final static String ATTRIBUT_LIST_MODULES = "modules";
+	private final static String ATTRIBUT_LIST_FILIERES = "filieres";
 
-	@PersistenceContext
-	private EntityManager entiryManager;
-	private FiliereRepository filiereRepository;
-	private SemestreRepository semestreRepository;
-	private ModuleRepository moduleRepository;
-	private EtapeRepository etapeRepository;
-	private HistoriqueRepository historiqueRepository;
+	private final static String ACTIVE = "mm-active";
+	private final static String ATTRIBUT_STRUCTURE_ENSEIGNEMENT = "StructureEnseignement";
+	
+	private final static String PAGE_LIST_ANNEES = "Structure/ListAnnees";
+	
+	private final static String REDIRECT_STRUCTURE_PAGE = "redirect:/inscription/StructureEnseignement";
+	@Autowired
+	private StructureInterface structure_metier;
 
-	public StructureController(InscriptionPedagogiqueModuleRepository inscriptionPedagogiqueModuleRepository,
-			EtapeRepository etapeRepository, ModuleRepository moduleRepository,
-			InscriptionPedagogiqueRepository inscriptionPedagogiqueRepository, EtudiantRepository studentRepository,
-			SemestreRepository semestreRepository, InscriptionAdministrativeRepository inscriptionAdministrative,
-			InscriptionEnLigneRepository inscriptionEnLigne, FiliereRepository filiereRepository,
-			HistoriqueRepository historiqueRepository) {
-		this.filiereRepository = filiereRepository;
-		this.semestreRepository = semestreRepository;
-		this.moduleRepository = moduleRepository;
-		this.etapeRepository = etapeRepository;
-		this.historiqueRepository = historiqueRepository;
-	}
 	// -------+++++++++-----------------++++++++------------++++++++----------++++++++-------//
 	//+*+*+*+*+*+*+*+*+*++*+*+*+*+*+**+*+*+*+*+*+*+*+*+*+/ PARTIE STRUCTURE D'ENSEIGNEMENT /*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+*+**+*+*+//
 		// -------+++++++++-----------------++++++++------------++++++++----------++++++++-------//
@@ -57,61 +37,34 @@ public class StructureController {
 	//------------------------------------------allez vers page structure d'enseignement--------------------------------------------------//
 
 		@GetMapping("/inscription/StructureEnseignement")
-		public ModelAndView structureDEnseignement() {
-			ModelAndView model = new ModelAndView("ListAnnees");
-			model.addObject("f", filiereRepository.getAllFiliere());
-			model.addObject("etape", etapeRepository.findAll());
-			model.addObject("semestres", semestreRepository.getAllSemestre());
-			model.addObject("modules", moduleRepository.findAll());
-			model.addObject("StructureEnseignement", "mm-active");
+		public ModelAndView structureDEnseignement() throws DataNotFoundExceptions{
+			ModelAndView model = new ModelAndView(PAGE_LIST_ANNEES);
+			model.addObject(ATTRIBUT_LIST_FILIERES, structure_metier.listerFilieres());
+			model.addObject(ATTRIBUT_LIST_ETAPES, structure_metier.listerEtapes());
+			model.addObject(ATTRIBUT_LIST_SEMESTRES, structure_metier.listerSemestres());
+			model.addObject(ATTRIBUT_LIST_MODULES, structure_metier.listerModules());
+			model.addObject(ATTRIBUT_STRUCTURE_ENSEIGNEMENT, ACTIVE);
 			return model;
 		}
 
 	//-------------------------------------modification d'une (plusieurs) années : diplomante ou pas------------------------------------//		
 
-		@PostMapping("/inscription/ModifierAnneeDiplomante")
-		public ModelAndView ModifierAnneeDiplomante(@RequestParam("id_ip") String ids) {
-			String idstring[] = ids.split(",");
-
-			List<Integer> id = new ArrayList<Integer>();
-			for (int i = 0; i < idstring.length; i++) {
-				id.add(Integer.parseInt(idstring[i]));
-			}
-			List<Etape> etapesA = etapeRepository.findAllById(id);
-			for (int i = 0; i < etapesA.size(); i++) {
-				etapeRepository.activerDiplomante(etapesA.get(i).getId_etape(), 1);
-				historiqueRepository.save(new Historique(
-						"année " + etapesA.get(i).getLibelle_etape() + " est desormait diplomante", new java.util.Date()));
-			}
-
-			List<Etape> etapesD = etapeRepository.findAll();
-			int trouver = 0;
-			for (int i = 0; i < etapesD.size(); i++) {
-				trouver = 0;
-				for (int j = 0; j < etapesA.size(); j++) {
-					if (etapesD.get(i) == etapesA.get(j)) {
-						trouver = 1;
-					}
-				}
-				if (trouver == 0) {
-					//etapeRepository.activerDiplomante(etapesD.get(i).getId_etape(), 0);
-					historiqueRepository.save(new Historique(
-							"année " + etapesD.get(i).getLibelle_etape() + " n'est desormait plus diplomante",
-							new java.util.Date()));
-				}
-			}
-			return new ModelAndView("redirect:/inscription/StructureEnseignement");
+		@PostMapping("/inscription/StructureEnseignement")
+		public ModelAndView ModifierAnneeDiplomante(@RequestParam("id_ip") String ids) throws EntityNotFoundException{
+			structure_metier.diplomerAnnee(ids);
+			return new ModelAndView(REDIRECT_STRUCTURE_PAGE);
 		}
 
 	//-------------------------------------------------Details semestre---------------------------------------------------//
 
 		@GetMapping("/inscription/VoirSemestre")
 		public ModelAndView VoirSemestre(@RequestParam("idS") int idS) {
-			ModelAndView model = new ModelAndView("voirSemestre");
+			/*ModelAndView model = new ModelAndView("voirSemestre");
 			Semestre semestre = semestreRepository.getOne(idS);
 			List<Module> modules = moduleRepository.getModuleBySemestre(semestre);
 			model.addObject("modules", modules);
 			model.addObject("semestre", semestre);
-			return model;
+			return model;*/
+			return null;
 		}
 }
--- DeliberationNewVersion/src/main/java/com/ziad/administrateur/structures/StructureService.java
+++ DeliberationNewVersion/src/main/java/com/ziad/administrateur/structures/StructureService.java
@@ -1,5 +1,86 @@
 package com.ziad.administrateur.structures;
 
-public class StructureService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import com.ziad.models.Etape;
+import com.ziad.models.Filiere;
+import com.ziad.models.Historique;
+import com.ziad.models.Modulee;
+import com.ziad.models.Semestre;
+import com.ziad.repositories.EtapeRepository;
+import com.ziad.repositories.FiliereRepository;
+import com.ziad.repositories.HistoriqueRepository;
+import com.ziad.repositories.ModuleRepository;
+import com.ziad.repositories.SemestreRepository;
+
+@Service
+@Primary
+public class StructureService implements StructureInterface {
+	@Autowired
+	private FiliereRepository filiereRepository;
+	@Autowired
+	private SemestreRepository semestreRepository;
+	@Autowired
+	private ModuleRepository moduleRepository;
+	@Autowired
+	private EtapeRepository etapeRepository;
+	@Autowired
+	private HistoriqueRepository historiqueRepository;
+
+	@Override
+	public List<Etape> listerEtapes() {
+		return etapeRepository.findAll();
+	}
+
+	@Override
+	public List<Filiere> listerFilieres() {
+		return filiereRepository.findAll();
+	}
+
+	@Override
+	public List<Modulee> listerModules() {
+		return moduleRepository.findAll();
+	}
+
+	@Override
+	public List<Semestre> listerSemestres() {
+		return semestreRepository.findAll();
+	}
+
+	@Override
+	public void diplomerAnnee(String ids) throws EntityNotFoundException {
+		String idstring[] = ids.split(",");
+
+		List<Long> id = new ArrayList<Long>();
+		for (int i = 0; i < idstring.length; i++) {
+			id.add(Long.parseLong(idstring[i]));
+		}
+		List<Etape> etapesA = etapeRepository.findAllById(id);
+		for (Etape etape : etapesA) {
+			Filiere filiere = etape.getFiliere();
+			List<Etape> etapes = filiere.getEtapes();
+			for (Etape etapeupdate : etapes) {
+				if (!etapesA.contains(etapeupdate)) {
+					etapeupdate.setDiplomante(false);
+				}
+			}
+			etapeRepository.saveAll(etapes);
+		}
+		Consumer<Etape> diplomer = etape -> {
+			etape.setDiplomante(true);
+			historiqueRepository.save(new Historique("année " + etape.getLibelle_etape() + " est desormait diplomante",
+					new java.util.Date()));
+		};
+		etapesA.stream().forEach(diplomer);
+
+	}
 
 }
