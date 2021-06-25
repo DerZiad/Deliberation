4348ebf26875c3a46ffb5487596f40fa813518dc
Ziad Bougrine <ziadbougrine@gmail.com>
2021-06-25 23:56:32 +0100

    Exception

--- DeliberationNewVersion/src/main/java/com/ziad/advices/EntryException.java
+++ DeliberationNewVersion/src/main/java/com/ziad/advices/EntryException.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
-/*
+
 @ControllerAdvice
 public class EntryException {
 	@ExceptionHandler(EntityNotFoundException.class)
@@ -15,4 +15,3 @@ public class EntryException {
 		return model;
 	}
 }
-*/
